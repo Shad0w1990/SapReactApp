c31e991410560b8c239d61aebad0290798a8052b
ehsan khorsand <e.khorsand@Hoshmand.local>
2019-07-03 17:13:40 +0430

    listdata

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import React from 'react';
 import './App.css';
 import Sample from './JsComponent/Sample';
 
-function App() {
+class App extends React.Component {
+ state={
+  lists:[
+  {id:"1",name:"ehsan khorsand" ,age:"28"},
+  {id:"2",name:"mohsen khorsand" ,age:"30"},
+  {id:"3",name:"behrooz gholamhosein fard" ,age:"25"}
+]
+};
+  render(){
+  
   return (
     <div className="App">
-      <Sample />
+      
+      <Sample lists={this.state.lists}/>
     </div>
   );
+}
 }
 
 export default App;
--- src/JsComponent/Sample.js
+++ src/JsComponent/Sample.js
@@ -1,18 +1,27 @@
 import React from 'react';
 class Sample extends React.Component {
-    state={
-        name:"ehsan khorsand",
-        age:28
-    }
-    render(){
-        return(
-<div>
-name is {this.state.name}
-<br/>
-age is {this.state.age}
-</div>
+    // state={
+    //     name:"ehsan khorsand",
+    //     age:28
+    // }
+  
+    render(){ 
+        
+         const {lists} = this.props;
 
-        );
+        const mytest=lists.map((test)=>{
+return(
+    <div className="test" key={test.id}>
+<div>
+    name:{test.name}
+</div>
+<div>
+age :{test.age}
+</div>
+</div>
+);
+        });
+        return <div className="mytest"> {mytest}</div> ;
     }
 }
 export default Sample;
